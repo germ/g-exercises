fbc262b6af28c5e0c1a5b58a303c24b0ca94e7c2
Jeremy Bernhardt <bernhardtjeremy@gmail.com>
2018-09-30 21:16:38 -0600

    Added in a few problems namely little man assembler and emulator

--- 97-Little-Man/assembler/97-Little-Man.go
+++ 97-Little-Man/assembler/97-Little-Man.go
@@ -2,39 +2,143 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
+	"strconv"
+	"strings"
 )
 
-type LittleMan struct {
-	PC          byte
-	Memory      [100]byte
-	Inbox       byte
-	Outbox      byte
-	Accumulator byte
+var menMap = map[string]int{
+	"ADD": 100,
+	"SUB": 200,
+	"STA": 300,
+	"LDA": 400,
+	"BRA": 500,
+	"BRZ": 600,
+	"BRP": 700,
+	"INP": 800,
+	"OUT": 901,
+	"HLT": 902,
+	"DAT": 000,
 }
 
 func main() {
-	fmt.Println("03: Little Man Computer Simulator")
+	fmt.Println("03: Little Man Computer Assembler")
+	var byteCode []int
+
+	// Did we get run properly?
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: ./97 program.hex")
+		fmt.Println("Usage: ./97 program.lmc")
+		fmt.Println("Creates fileName.hex")
 		return
 	}
 
+	// Read entire file and split
+	f, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	byteCode = assembleSource(string(f))
+	// Convert to easily parseable format (gob)
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	enc.Encode(byteCode)
+
+	// Write our output
+	filePath := path.Base(os.Args[1]) + ".hex"
+	err = ioutil.WriteFile(filePath, buf.Bytes(), 0755)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("Compilation sucessful, byteCode written to %v", filePath)
 }
 
-// Load bytecode to memory
-func (l *LittleMan) LoadProgram(pgm []byte) {
-}
+// Read the source and return bytecode suitable for running
+func assembleSource(srcIn string) (byteCode []int) {
+	// So there's a few things we need to do. The first is to strip any comments or blank
+	// lines out. Then we scan over the source and make a list of the address of any labels
+	// we then substitute any labels with those offsets, lastly we convert the generated code
+	// Into bytecode. Woo!
 
-// Advance execution
-func (l *LittleMan) Step() {
-}
+	// Step one: Remove comments
+	src := strings.Split(srcIn, "\n")
+	label := make(map[string]int)
 
-// Place byte into Inbox
-func (l *LittleMan) Input() {
-}
+	var tmp []string
+	for _, v := range src {
+		// Blank line
+		if len(v) == 0 {
+			continue
+		}
 
-// Retrieve byte from Inbox
-func (l *LittleMan) Output() {
+		// Comment
+		if strings.HasPrefix(v, ";") {
+			continue
+		}
+
+		tmp = append(tmp, v)
+	}
+	src = tmp
+
+	// Step 2: Locate any labels, these will be in position one and not a instruction
+	for i, v := range src {
+		f := strings.Fields(v)
+		// Check against memnomics
+		if _, ok := menMap[f[0]]; !ok {
+			label[f[0]] = i
+		}
+	}
+
+	// Step 3: Loop over removing label defs and replacing refrences
+	var processedSrc [][]string
+	for _, v := range src {
+		line := strings.Fields(v)
+
+		// Remove the label def if found
+		if _, ok := label[line[0]]; ok {
+			line = line[1:]
+		}
+
+		//check operand for label and sub
+		if len(line) > 1 {
+			if v, ok := label[line[1]]; ok {
+				line[1] = fmt.Sprintf("%d", v)
+			}
+		}
+
+		processedSrc = append(processedSrc, line)
+	}
+
+	// Step 4: Convert to bytecode, 1 byte per ins/datum
+	for i, v := range processedSrc {
+		// Retrieve instruction code
+		ins, ok := menMap[v[0]]
+		if !ok {
+			fmt.Printf("Error Processing line %v: %v", i, v)
+			panic("")
+		}
+
+		// Handle dataless instructions
+		if v[0] == "INP" || v[0] == "OUT" || v[0] == "HLT" {
+			byteCode = append(byteCode, ins)
+			continue
+		}
+
+		// Trim data and add
+		data, err := strconv.Atoi(v[1])
+		if err != nil {
+			fmt.Printf("Invalid data on line %v: %v ", i, err)
+			panic("")
+		}
+		data = ins + data
+
+		byteCode = append(byteCode, data)
+	}
+	return
 }
--- 97-Little-Man/emulator/97-Little-Man.go
+++ 97-Little-Man/emulator/97-Little-Man.go
@@ -2,39 +2,186 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/gob"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
-type LittleMan struct {
-	PC          byte
-	Memory      [100]byte
-	Inbox       byte
-	Outbox      byte
-	Accumulator byte
+var menMap = map[int]string{
+	100: "ADD",
+	200: "SUB",
+	300: "STA",
+	400: "LDA",
+	500: "BRA",
+	600: "BRZ",
+	700: "BRP",
+	800: "INP",
+	901: "OUT",
+	902: "HLT",
+	000: "DAT",
+}
+
+type littleMan struct {
+	PC          int
+	Memory      [100]int
+	Inbox       int
+	Outbox      int
+	Accumulator int
+}
+
+var stdin *bufio.Reader
+var stdout *bufio.Writer
+
+var DEBUG bool
+var VERBOSE bool
+
+func init() {
+	var showHelp bool
+	// Set up buffered IO
+	stdin = bufio.NewReader(os.Stdin)
+	stdout = bufio.NewWriter(os.Stdout)
+
+	// Parse flags
+	flag.BoolVar(&DEBUG, "d", false, "Dumps state on Halt")
+	flag.BoolVar(&VERBOSE, "v", false, "Dumps state after every step")
+	flag.BoolVar(&showHelp, "h", false, "Show help")
+	flag.Parse()
+
+	// Display usage and exit
+	if len(flag.Args()) != 1 || showHelp {
+		fmt.Println("Usage: ./97 program.hex")
+		fmt.Println("Input is read from STDIN, Output written on exit")
+		flag.PrintDefaults()
+		return
+	}
 }
 
 func main() {
 	fmt.Println("03: Little Man Computer Simulator")
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: ./97 program.hex")
-		return
+	var comp littleMan
+
+	// Load pgm into computer
+	comp.LoadProgram(os.Args[1])
+	// Exec loop
+	for {
+		isHalted, err := comp.Step()
+		if VERBOSE {
+			comp.Dump()
+		}
+
+		if isHalted {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
 	}
 
+	fmt.Println("Execution Terminated.")
+	if DEBUG || VERBOSE {
+		comp.Dump()
+	}
 }
 
 // Load bytecode to memory
-func (l *LittleMan) LoadProgram(pgm []byte) {
+func (l *littleMan) LoadProgram(path string) (e error) {
+	// Get raw data
+	raw, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Printf("Could not read file: %v", err)
+		return err
+	}
+
+	// Decode to parsable format
+	var pgm []int
+	buf := bytes.NewBuffer(raw)
+	dec := gob.NewDecoder(buf)
+	err = dec.Decode(&pgm)
+	if err != nil {
+		fmt.Printf("Error decoding file: %v", err)
+		return err
+	}
+
+	// Trim and store
+	for i, v := range pgm {
+		if i == 100 {
+			fmt.Println("Warning! Program is larger then avalible memory")
+			break
+		}
+
+		l.Memory[i] = v
+	}
+
+	return
 }
 
 // Advance execution
-func (l *LittleMan) Step() {
+func (l *littleMan) Step() (isHalt bool, err error) {
+	ins, data := parseCode(l.Memory[l.PC])
+	l.PC++
+
+	switch ins {
+	case "ADD":
+		l.Accumulator += l.Memory[data]
+	case "SUB":
+		l.Accumulator -= l.Memory[data]
+	case "STA":
+		l.Memory[data] = l.Accumulator
+	case "LDA":
+		l.Accumulator = l.Memory[data]
+	case "BRA":
+		l.PC = data
+	case "BRZ":
+		if l.Accumulator == 0 {
+			l.PC = data
+		}
+	case "BRP":
+		if l.Accumulator != 0 {
+			l.PC = data
+		}
+	case "INP":
+		c, err := stdin.ReadByte()
+		if err != nil {
+			fmt.Printf("Error reading input. %v", err)
+			fmt.Println("Halting")
+			return true, err
+		}
+
+		l.Accumulator = int(c)
+	case "OUT":
+		err = stdout.WriteByte(byte(l.Accumulator))
+		if err != nil {
+			fmt.Printf("Error writig output. %v", err)
+			fmt.Println("Halting")
+			isHalt = true
+		}
+	case "HLT":
+		isHalt = true
+	}
+	return
 }
 
-// Place byte into Inbox
-func (l *LittleMan) Input() {
+func (l *littleMan) Dump() {
+	fmt.Println("System Dump: ")
+	fmt.Printf("PC: %v Inbox: %v Outbox: %v Acc: %v\n\n", l.PC, l.Inbox, l.Outbox, l.Accumulator)
+	fmt.Println("Memory Dump:")
+
+	// Pretty hex dump the core mem
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%v ] ", i)
+		for j := 0; j < 10; j++ {
+			fmt.Printf("%04x ", l.Memory[i*10+j])
+		}
+		fmt.Printf("\n")
+	}
 }
 
-// Retrieve byte from Inbox
-func (l *LittleMan) Output() {
+// Seperate the instruction from its operand
+func parseCode(code int) (ins string, data int) {
+	return
 }
